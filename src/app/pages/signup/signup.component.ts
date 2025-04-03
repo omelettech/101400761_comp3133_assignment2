36a750e37b021da4aac05bb5185c28ef4ee25c17
Awsaf Fida Mahmud <fidaawsaf@gmail.com>
2025-04-03 18:41:38 -0400

    Added login and signup component

---
+++ src/app/pages/signup/signup.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-signup',
+  imports: [],
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.css'
+})
+export class SignupComponent {
+  signupForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.signupForm = this.fb.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSignup() {
+    if (this.signupForm.valid) {
+      console.log('Signing up with:', this.signupForm.value);
+    }
+  }
+}
